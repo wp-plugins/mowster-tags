a5153e26563c638307f7d83a81acc40ecc0e51fe
PedroDM <PedroDM@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-18 12:34:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mowster-tags/tags/1.14@411388 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function($) {  
+		
+		var myArray=["5", "10", "15", "20", "30", "40", "50", "60", "70", "80", "90", "100"];
+		var mowsterTags_select_options = '';
+		
+		for(i=0; i < myArray.length; i++){
+			if (myArray[i] == mowsterVars.mowsterTags_countTags) {
+				var mowsterTags_select_options = mowsterTags_select_options + '<option value="' + myArray[i] + '" selected>' + myArray[i] + '</option>';
+			} else mowsterTags_select_options = mowsterTags_select_options + '<option value="' + myArray[i] + '">' + myArray[i] + '</option>'; 
+		}
+         
+        jQuery(mowsterVars.mowsterTags_html_add_to).prepend("<div style=\"margin-bottom: 5px;\"><select name=\"mowsterTags_count\" id=\"mowsterTags_count\">" + mowsterTags_select_options + "</select> <input type=\"button\" class=\"button\" value=\"" + mowsterVars.mowsterTags_fetchTags + "\" id=\"mowsterTags\" style=\"vertical-align: middle;\" /> " +
+        " <a id=\"jobs\" href=\"http://jobs.mowster.net/\" target=\"_blank\"><img src=\"" + mowsterVars.mowsterTags_logo_path + "\" alt=\"jobs.mowster.net\" title=\"jobs.mowster.net\" style=\"vertical-align: middle; margin-left: 3px;\" /></a></div>");
+
+		jQuery("#mowsterTags").click(function(e) {
+		
+			var content = (typeof tinyMCE == "undefined" || 
+                              typeof tinyMCE.getInstanceById("content") == "undefined" ||
+                              tinyMCE.getInstanceById("content").isHidden()) ?
+                              "<div>" + jQuery("#content").val() + "</div>" : 
+                              tinyMCE.getInstanceById("content").getContent();
+							  
+			var text = jQuery("#title").val() + " " + 
+                          (typeof content === 'string' ? jQuery(content).text() : "") + " " +
+                          jQuery("#excerpt").val();
+			
+			var tags = jQuery(mowsterVars.mowsterTags_newtags).val();
+		
+			
+			
+			if (typeof text === 'string') {
+			
+				if (text.length < 20) {
+					alert(mowsterVars.mowsterTags_insuficient_text);
+					jQuery("#mowsterTags").val(mowsterVars.mowsterTags_fetchTags);
+					return true;
+				}
+			
+				jQuery(this).val(mowsterVars.mowsterTags_fetchingTags);
+				var count = jQuery("#mowsterTags_count").val();
+				
+				jQuery.ajax({
+					type: "POST",
+					dataType: "text",
+					async: true,
+					timeout: 15000,
+					data: { 
+						action: 'join_post_tags', text: text, count: count, tags: tags 
+					},
+					dataFilter: function(data, type) {
+						return data.replace(/<\/?[^>]+>/gi, "");
+					},         			   
+					url: mowsterVars.mowsterTags_ajax_path,
+					success: function(data, textStatus) {
+						jQuery(mowsterVars.mowsterTags_newtags).focus();
+						jQuery(mowsterVars.mowsterTags_newtags).val(data);
+					},
+					error: function (request, textStatus) {
+						if ("timeout" == textStatus) {
+						   alert(mowsterVars.mowsterTags_server_error);
+						} else {
+						   alert(mowsterVars.mowsterTags_misc_error);
+						}
+					},
+					complete: function(request, textStatus) {
+					jQuery("#mowsterTags").val(mowsterVars.mowsterTags_fetchTags); 
+					}
+				});	
+			}
+		});
+});
