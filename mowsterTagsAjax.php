a585f4836b6776df7a4d81087e906a71cc164854
PedroDM <PedroDM@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-20 11:15:06 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mowster-tags/tags/1.22@533940 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mowsterTagsAjax.php
@@ -0,0 +1,76 @@
+<?php
+
+if (realpath(__FILE__) === realpath($_SERVER["SCRIPT_FILENAME"])) {
+	$location = 'http://'.substr($_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"], 0, strrpos($_SERVER["SERVER_NAME"].'/'.$_SERVER["REQUEST_URI"], '/wp-content/'));
+	header('Location: '.$location);
+	die();
+}
+
+function join_post_mowsterTags(){
+
+	// Save count option
+	$title = strip_tags(stripslashes($_POST['title']));
+	$text = strip_tags(stripslashes($_POST['text']));
+	$count = strip_tags(stripslashes($_POST['count']));
+	$tags = strip_tags(stripslashes($_POST['tags']));
+	
+	if (get_option('mowsterTags_count') != $count) update_option('mowsterTags_count', $count);
+
+		
+	// Get data
+	$content = trim(stripslashes($text) .' '. stripslashes($title));
+
+	if (empty($content)) {
+		echo $tags;
+		die();
+	}
+
+			
+	// Build params
+	$param = 'appid=h4c6gyLV34Fs7nHCrHUew7XDAU8YeQ_PpZVrzgAGih2mU12F0cI.ezr6e7FMvskR7Vu.AA--'; // Yahoo ID
+	$param .= '&context='.urlencode($content); // Post content
+	if (!empty($tags)) {
+		$param .= '&query='.urlencode($tags); // Existing tags
+	}
+	$param .= '&output=json'; 
+			
+	// Get PHP Array
+	$data = array();
+	$reponse = wp_remote_post('http://search.yahooapis.com/ContentAnalysisService/V1/termExtraction', array('body' =>$param) );
+	if( !is_wp_error($reponse) && $reponse != null ) {
+		if (wp_remote_retrieve_response_code($reponse) == 200) 
+			$data = maybe_unserialize(wp_remote_retrieve_body($reponse));
+	}
+	$data = json_decode($data);
+			
+	if (empty($data) || empty($data->ResultSet->Result) || is_wp_error($data)) {
+		echo $tags;
+		die();
+	}
+		
+		
+	// Remove empty terms
+	$data = array_unique($data->ResultSet->Result);
+	$data = array_filter($data, 'mowsterTags_callback');
+
+	$check_tags = explode(',', str_replace(' ', '', $tags));
+	
+	foreach ($data as $term) {		
+		if (!in_array($term, $check_tags)) $display .= esc_html($term).',';
+		$counter++;
+		if ($counter == $count) break;
+	}
+	
+	if ($tags && substr($tags,-1) != ',') $tags=$tags.",";
+
+	echo $tags.substr($display,0,-1);
+	die();
+	
+}
+
+// fix: PHP versions not supporting anonymous functions
+function mowsterTags_callback($x) { 
+	return (strlen($x) > 2); 
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,87 @@
+=== mowsterTags ===
+Contributors: PedroDM
+Tags: tag, tags, tagging, post, suggest, related, automatic 
+Requires at least: 3.0
+Tested up to: 3.3
+Stable tag: 1.22
+
+Wordpress plugin for tag suggestions using Yahoo API
+
+== Description ==
+
+With this plugin, you will be able to use the Yahoo API terms to get suggested tags for your posts. 
+
+* Just click on `Fetch tags`. Title, content and optional excerpt of your post will be analyzed by Yahoo in order to find words that could be useful as tags. 
+
+It currently works with `all languages supported by Yahoo! Term Extraction`. 
+
+Plugin site <a href="http://development.mowster.net">http://development.mowster.net</a>
+
+**Others Credits**
+<a href="http://jobs.mowster.net">http://jobs.mowster.net</a>
+
+== Installation ==
+
+1. Download the mowster-Tags.zip file to your computer.
+1. Unzip the file.
+1. Upload `mowster-Tags` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= Which languages are supported? =
+
+All supported by Yahoo! Term Extraction
+
+== Screenshots ==
+
+1. Button to `Fetch tags`.
+
+== Changelog ==  
+
+= 1.22 =
+* URL optimization
+* Folders organization
+
+= 1.21 =
+* Security improvement
+
+= 1.20 =
+* Security improved
+* Spanish and Portuguese translation added
+
+= 1.18 =
+* minor improvals to avoid conflicts with others plugins
+
+= 1.17 =
+* Security fix
+
+= 1.16 =
+* bug fixed in callback function
+
+= 1.15 =
+* minor improval, hooks only loaded in new-page script
+
+= 1.14 =
+* Hooks improved, performance optimized
+
+= 1.13 =
+* bug fixed jQuery Fetch button
+
+= 1.12 =
+* jQuery code optimized
+
+= 1.11 =
+* bug fixed for PHP versions (< 5.3.0) that does not support anonymous functions
+
+= 1.1 =
+* Drop-down limit fixed
+* jQuery code optimized
+
+= 1.0 =
+* Initial release
+* Added localization (English, Portuguese)
+* Tested with Internet Explorer, Firefox, Safari and Opera
+
+
+== Upgrade Notice ==
